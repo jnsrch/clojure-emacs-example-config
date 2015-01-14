56dae5897d6b4ac3a409cf6f3fe28e4c3f605b68
Hello Funk <hellofunk@floatingmachine.com>
2015-01-14 09:48:42 +0100

    customizations

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
+This is my own personal emacs setup which is 100% compatible with the official [clojure-emacs/example-config](https://github.com/clojure-emacs/example-config) repository, which provides a minimal working emacs configuration for Clojure/Clojurescript development, with all the latest features from the clojure-emacs team.  
+
+This personal repository adds the following:
+
+* My own user-customizations.el file. This file is the upstream repo's starting point for your own customizations that lay on top of the original example-config from clojure-emacs.
+* One particular tool, the undo-tree, requires an extra .el script, and it is placed in a directory called `hellofunk-config` to isolate it from the other .el scripts that are a part of the original upstream repo at clojure-emacs. If you aren't interested in this feature, you can exclude this directory and remove the relevant installation line from the user-customizations.el file (near the bottom of the file).
+* View all customizations, with comments about their usage and links to their home pages, in this repo's [user-customizations.el file](https://github.com/hellofunk/example-config/blob/master/user-customizations.el).
+* This repo also checks in the downloaded Melpa packages, which appear in the elpa/ directory. The upstream emacs config will automatically download these anyway when you first run emacs, so this directory is not necessary, but since I push my own emacs setup back to this repo, I chose to include it anyway as a backup.
+
+ *All other settings in this config are part of the upstream repo from clojure-emacs.*
+
+I plan to keep this repo up-to-date in sync with the official clojure-emacs config, though really all that is needed to add these features to the master upstream repo is the user-customizations.el file and the undo-tree script, if desired.
+
+###Known Issues
+
+This is a stable and working config for several members who have tested it. The following 2 issues have appeared which are generally trivial and can be ignored; all the same, they are listed here so you don't wonder what is going on:
+
+* You might see this error upon installation, regarding one particular library: `Warning (emacs): Unable to activate package `pkg-info'. Required package `dash-1.6.0' is unavailable`  This often appears for users, though actually `dash` does in fact get installed as it is required by the Clojure tools and they work fine. You can ignore it.
+* For one user, the first installation of this package resulted in a crash of Emacs. Upon a restart, everything was fine. This occurred during the many downloads from Melpa. 
+
+Here is the original Readme from the clojure-emacs team:
+
 # Example Config
 
 An example Emacs configuration for Clojure development with CIDER and clj-refactor.
@@ -41,8 +63,6 @@ Most users will eventually want more in their emacs config than the clojure-spec
 (require-package 'rainbow-delimiters)
 
 This would be all that is needed for emacs to automatically download the Rainbow Delimiters package from Melpa. Additional configs of any kind could be added to this user-customizations.el file. If the file is ommitted, no problem, no customizations are run.
-
-######Need Inspiration? [Here is one example of a user-customizations.el in action](https://github.com/hellofunk/example-config)
 
 ## Rationale
 
---
+++ user-customizations.el
@@ -0,0 +1,134 @@
+;This is the custom emacs config for user hellofunk, which adds features not included in the example-config from clojure-emacs.
+;info: https://github.com/hellofunk/example-config
+
+;anything you see below that uses require-package is downloaded automatically from Melpa and requires no manual installation of any .el files
+
+;----------------------------------------------------------------------------
+
+;turn off emacs alarms (those annoying beeps)
+(setq ring-bell-function 'ignore)
+
+;expand-region functionality is really great for lisp/clojure editing
+;from https://github.com/magnars/expand-region.el
+(require-package 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;sets the auto-save directory to be other than project directories, so you aren't inundated with tons of backup files
+(setq backup-directory-alist `(("." . "~/.saves")))
+
+;adds color matching for open and closed parenthesis
+;from https://github.com/Fanael/rainbow-delimiters
+(require-package 'rainbow-delimiters)
+;turns on this for default with clojure files
+(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+
+;be kind to your eyes, solarize! hey, that rhymes!
+;from https://github.com/bbatsov/solarized-emacs
+(require-package 'solarized-theme)
+(require 'solarized-light-theme)
+(provide 'solarized-theme)
+
+;use ido-mode, a must-have for quick emacs navigation
+;from http://emacswiki.org/emacs/InteractivelyDoThings
+;more about it: http://www.masteringemacs.org/article/introduction-to-ido-mode
+(require 'ido)
+(ido-mode t)
+
+;smex brings the power of ido mode to your M-x mini-buffer, another way to supercharge your emacs navigation
+;from https://github.com/nonsequitur/smex
+(require-package 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+; because M-S-6 is awkward
+(global-set-key (kbd "C-c q") 'join-line)
+
+;use I-Buffer
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;shortcut to launch a shell
+(global-set-key (kbd "C-x C-m") 'shell)
+
+;useful little tweak. press C-space once to start a mark, then C-space to end a mark. If you do this in succession, it will simply mark a particular point in a file. Then use C-u and repeat C-space to move back through your saved points.
+(setq set-mark-command-repeat-pop t)
+
+;turn off the emacs toolbar; we're coders, after all. when do we use a mouse?
+(tool-bar-mode -1)
+
+;turn off the scroll bars
+(scroll-bar-mode -1)
+
+;turn off the cursor blinking -- personally i find it distracting
+(blink-cursor-mode 0)
+
+;turn off text wrapping in the middle of a word
+(global-visual-line-mode 1)
+
+;set font size on startup
+;my eyes are kinda bad so i prefer a larger font size
+(set-face-attribute 'default nil :height 135)
+
+;; Font size
+(define-key global-map (kbd "C-c C-=") 'text-scale-increase)
+(define-key global-map (kbd "C-c C--") 'text-scale-decrease)
+
+;use shift + arrow keys to jump between windows
+(windmove-default-keybindings)
+
+;;these are simple keybindings i've always like to quickly make an emacs window taller, shorter, narrower or wider, without using the mouse
+(defalias 'hellofunk/windowtaller  (read-kbd-macro "C-u 2 C-x ^"))
+(global-set-key (kbd "C-q") 'hellofunk/windowtaller)
+(defalias 'hellofunk/windowshorter (read-kbd-macro "C-u - 2 C-x ^"))
+(global-set-key (kbd "C-z") 'hellofunk/windowshorter)
+(defalias 'hellofunk/windowwider (read-kbd-macro "C-u - 2 C-x }"))
+(global-set-key (kbd "C-\\") 'hellofunk/windowwider)
+(defalias 'hellofunk/windownarrower (read-kbd-macro "C-u - 2 C-x {"))
+(global-set-key (kbd "C-]") 'hellofunk/windownarrower)
+
+;;ORG MODE
+;;org mode is a great emacs feature for organization notes, outlines, and more. plenty of docs online about it
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+
+;erc configurations, for using irc. if using this, change the username from hellofunk to whatever you use
+(setq erc-nick "your-irc-username")
+;http://www.emacswiki.org/emacs/ErcLogging
+;choose any directory on your computer for placing log files
+;be mindful that some channels to do not promote irc logging, therefore don't post logs publicly without some knowledge about the manners of a channel
+(setq erc-log-channels-directory "~/your-irc-saves/")
+(setq erc-save-buffer-on-part t
+      erc-save-queries-on-quit t
+      erc-log-write-after-send t
+      erc-log-mode t
+      erc-autojoin-mode t
+      erc-log-write-after-insert t
+      erc-log-file-coding-system 'utf-8)
+(setq erc-log-insert-log-on-open nil)
+;choose your desired channels to automatically join here; anything can be added after the "#freenode.net" element
+(setq erc-autojoin-channels-alist '(("freenode.net"  "#emacs")))
+;hides annoying irc messages unrelated to actual chats
+(setq erc-hide-list (quote ("JOIN" "PART" "QUIT" "NICK" "AWAY")))
+(setq erc-modules (quote (autojoin button completion fill irccontrols list log match menu move-to-prompt netsplit networks noncommands readonly ring stamp track)))
+(defun erc-columns ()
+  (erc-fill-disable))
+(add-hook 'erc-mode-hook 'erc-columns )
+
+;adds a custom directory where some .el scripts are placed used in this config file. Note that if you have .el scripts that exist on Melpa, you don't need this directory. But I have an undo-tree script (configured below) not on Melpa, so this is how you do it:
+(add-to-list 'load-path (concat user-emacs-directory "hellofunk-config"))
+
+;undo-tree
+;from http://www.emacswiki.org/emacs/UndoTree
+;script must exist in the hellofunk-config directory before proceeding (see above paragraph)
+(require 'undo-tree)
+(global-undo-tree-mode)
+
+;see: http://www.emacswiki.org/emacs/EmacsForMacOS#toc26
+(when (eq system-type 'darwin) ;; mac specific settings
+  ;(setq mac-option-modifier 'alt)
+  (setq mac-command-modifier 'control)
+  ;(global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
+)
